959bfa2760834d8138a767e4c280ab9a320080ea
Hwang-Sangjin <aosdb5640@naver.com>
2021-01-22 02:17:58 +0900

    Update

--- baseball_game.py
+++ baseball_game.py
@@ -307,38 +307,64 @@ def is_no(one_more_input):
 
 def main():
     print("Play Baseball")
-    user_input = 999
-    EndGame = False
-    while EndGame is False:
-        strikeAndBalls = [0, 0]
+    user_input = 123
+    
+    # ===Modify codes below=============
+    # 위의 코드를 포함하여 자유로운 수정이 가능함
+
+    #print(is_digit(1032))
+    #print(is_between_100_and_999(102))
+    #print(is_duplicated_number(133))
+    #print(is_validated_number(133))
+    #print(get_not_duplicated_three_digit_number())
+    #print(get_strikes_or_ball(user_input,random_number))
+    #print(is_no("No"))
+    break_the_loop1 = False
+    break_the_loop2 = False
+    while break_the_loop1 is False:
         random_number = str(get_not_duplicated_three_digit_number())
         print("Random Number is : ", random_number)
-        # ===Modify codes below=============
-        # 위의 코드를 포함하여 자유로운 수정이 가능함
-        while strikeAndBalls[0] < 3: #스트라이크가 3개 미만일 때에만
+        while True:
             user_input = input("Input guess number : ")
-            if user_input == "0":
-                EndGame = True
+
+            if user_input == '0':
+                break_the_loop1 = True
                 break
+
             elif is_validated_number(user_input):
-                strikeAndBalls = get_strikes_or_ball(user_input, random_number)
-                print("Strikes : ", strikeAndBalls[0], " , Balls : ", strikeAndBalls[1])
+                temp_state = get_strikes_or_ball(user_input,random_number)
+
+                print("Strikes : %d , Balls : %d" %(temp_state[0],temp_state[1]))
+
+                if temp_state[0] == 3:
+                    while True:
+                        answer = input("You win, one more(Y/N)?")
+                        if is_yes(answer):
+                            break_the_loop2 = True
+                            break
+                        elif is_no(answer):
+                            break_the_loop2 = True
+                            break_the_loop1 = True
+                            break
+                        else:
+                            print("Wrong Input, Input again")
+
+                
+                
+                if break_the_loop2 == True:
+                    break
+
             else:
                 print("Wrong Input, Input again")
 
-        while EndGame is False:
-            OneMore = input("You win, one more(Y/N) ?")
-            if is_no(OneMore):
-                EndGame = True
-                break
-            elif is_yes(OneMore):
-                break
-            else:
-                print("Wrong Input, Input again")
+        if break_the_loop1 == True:
+            break
+
+    
+
     # ==================================
     print("Thank you for using this program")
     print("End of the Game")
-
 
 if __name__ == "__main__":
     main()
